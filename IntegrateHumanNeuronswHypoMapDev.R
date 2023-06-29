040c56d79da601669041aff552b23246df1af63a
Hannah Glover <69874757+Hannahglover@users.noreply.github.com>
2023-06-29 11:26:49 -0400

    Clean code and remove HypoMap non-neruons

--- IntegrateHumanNeuronswHypoMapDev.R
+++ IntegrateHumanNeuronswHypoMapDev.R
@@ -1,21 +1,60 @@
 library(Seurat)
 library(tidyverse)
 
-RomanovDev = readRDS("RomanovDevNeuronsHuman.rds")
-KimDev = readRDS("KimDevNeuronsHuman.rds")
+
+#HUMAN DATA
 EdKaZhou = readRDS("EdKaZhouHypoNeurons_mt10_integrated.rds")
 EdKaZhou@meta.data$Dataset = ifelse(EdKaZhou@meta.data$Age %in% c(29, 42, 50), "Siletti", "Zhou")
 EdKaZhou@meta.data$Dataset = ifelse(EdKaZhou@meta.data$sample %in% c("CS22_2_hypo", "CS22_hypo", "GW16_hypo", "GW18_hypo", "GW20_34_hypo", "GW22T_hypo1", "GW25_3V_hypo", "GW19_hypo"), "Herb", EdKaZhou@meta.data$Dataset)
+DefaultAssay(EdKaZhou) = "RNA"
 EdKaZhouNeurons = SplitObject(EdKaZhou, split.by = "Dataset")
-rm(EdKaZhou)
-hypoMap = readRDS("hypoMapHuman.rds")
-#hypoMap = readRDS("~/Dropbox/Columbia/Sci Adv/hypoMapHuman.rds")
+
+
+#HYPOMAP DATA
+hypoMap = readRDS("~/hypoMap.rds")
+hypoMap = ConvertGeneNames(hypoMap, reference.names = row.names(Neuro.combined), homolog.table = "https://seurat.nygenome.org/azimuth/references/homologs.rds")
+#saveRDS(hypoMap, "~/hypoMapHuman.rds")
+#hypoMap = readRDS("hypoMapHuman.rds")
 Idents(hypoMap) = "Dataset"
 hypoMap = subset(hypoMap, idents = c("Dowsett10xnuc", "RomanovDev10x", "KimDev10x"), invert=T)
+Idents(hypoMap) = "C2_named"
+hypoMap = subset(hypoMap, idents = c("C2-1: Neurons"))
 hypoMapData = SplitObject(hypoMap, split.by = "Dataset")
-rm(hypoMap)
+
+#KIM DATA
+Kim_Meta = read.csv("~/Downloads/GSE132355_E10-P45_umap_data.csv")
+Kim_MetaNeurons = subset(Kim_Meta, Kim_Meta$Cluster %in% c("GE/POA", "VMH", "POA/SCN", "SMN", "PVH & SON", "MMN", "PMN", "LH", "ARC", "POA", "SCN", "DMH/ID", "Hypothalamic neurons"))
+Kim_Data = readRDS("~/Downloads/GSE132355_E10-P45_log2cpm.rds")
+Kim_Data = CreateSeuratObject(Kim_Data, project= "KimDev")
+Kim_Data = subset(Kim_Data, cells = Kim_MetaNeurons$X.1)
+DefaultAssay(Kim_Data) = "RNA"
+Kim_Data = ConvertGeneNames(Kim_Data, reference.names = row.names(EdKaZhou), homolog.table = "https://seurat.nygenome.org/azimuth/references/homologs.rds")
+Kim_Data@meta.data$Dataset = "KimDev"
+#saveRDS(Kim_Data, "~/KimDevNeuronsHuman.rds")
+#KimDev = readRDS("KimDevNeuronsHuman.rds")
+
+
+#ROMANOV DATA
+Romanov_Data = readRDS("~/Downloads/GSE132730_TractNAE_integrated.rds")
+Idents(Romanov_Data) = "Class"
+Romanov_Data = subset(Romanov_Data, idents = "Neurons")
+DefaultAssay(Romanov_Data) = "RNA"
+Romanov_Data = ConvertGeneNames(Romanov_Data, reference.names = row.names(EdKaZhou), homolog.table = "https://seurat.nygenome.org/azimuth/references/homologs.rds")
+Romanov_Data@meta.data$Dataset = "RomanovDev"
+#saveRDS(Romanov_Data, "~/RomanovDevNeuronsHuman.rds")
+#RomanovDev = readRDS("RomanovDevNeuronsHuman.rds")
+
+
+#TRANSFER HYPOMAP DATA TO HUMAN
+Hypo.anchors <- FindTransferAnchors(reference = hypoMap, query = EdKaZhou,  dims = 1:30)
+predictions <- TransferData(anchorset = Hypo.anchors, refdata = list("Dataset" = hypoMap$Dataset, "C2_named" = hypoMap$C2_named, "C7_named" = hypoMap$C7_named, "C25_named" = hypoMap$C25_named, "C66_named" = hypoMap$C66_named, "C185_named" = hypoMap$C185_named, "C286_named" = hypoMap$C286_named, "C465_named" = hypoMap$C465_named, "Region_predicted" = hypoMap$Region_predicted, "Region_summarized" = hypoMap$Region_summarized),   dims = 1:30)
+EdKaZhou <- AddMetaData(EdKaZhou, metadata = predictions)
+saveRDS(predictions, "predictions_refined.rds")
+
 
 Hypo.list = c("Romanov" = RomanovDev, "Kim" = KimDev, EdKaZhouNeurons, hypoMapData)
+rm(hypoMap)
+rm(EdKaZhou)
 
 for(n in names(Hypo.list)){
   DefaultAssay(Hypo.list[[n]]) = "RNA"
@@ -36,7 +75,7 @@ for(n in names(Hypo.list)){
 Hypo.anchors <- FindIntegrationAnchors(object.list = Hypo.list, anchor.features = feat, reduction = "rpca")
 Hypo.combined <- IntegrateData(anchorset = Hypo.anchors)
 
-#saveRDS(Hypo.combined, "~/HypoMapHUMAN.rds")
+#saveRDS(Hypo.combined, "~/HypoMapHUMANDev.rds")
 
 Hypo.combined = ScaleData(Hypo.combined, vars.to.regress = "percent.mt")
 Hypo.combined = RunPCA(Hypo.combined)
